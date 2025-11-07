2e757ccabd899abe72e7dfa3dfe07a76b15aa27d
ImanKhodamoradiany <Imankhodamoradiany@gmail.com>
2025-11-07 17:08:12 +0330

    Create animation For hover and Active Link NavBar

--- src/component/Ui/Moloculs/LinkNavBar/LinkNavBar.jsx
+++ src/component/Ui/Moloculs/LinkNavBar/LinkNavBar.jsx
@@ -1,20 +1,23 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import Button from "../../Atoms/Button/Button";
 import { NavBarList } from "../../../../Core/constans/Array";
 
 function LinkNavBar() {
+    const Pathnam = useLocation()
+    console.log(Pathnam)
     return (
         <div>
             <ul className="flex justify-center items-center gap-14">
                 {NavBarList.map((item) => (
                     <li key={item.key}>
-                        <Link className="text-[1vw]" to={item.Link}>{item.title}</Link>
+                        <Link className={`text-[1vw] duration-500 ${Pathnam.pathname == item.Link ? "NavBarAnimationAvtive text-[#1DAEFF]" : "NavBarAnimation"} hover:text-[#1DAEFF] relative`} 
+                        to={item.Link}>{item.title}</Link>
                     </li>
                 ))}
                 <li>
                     <Link to={"/LoginPage"}>
                         <Button
-                            className={"text-[#1DAEFF] border-2 px-9 py-2 rounded-xl border-[#1DAEFF] text-[1vw] transition-all duration-300 hover:text-white hover:bg-[#1DAEFF]"}>
+                            className={` ${Pathnam.pathname == '/LoginPage' ? 'text-white bg-[#1DAEFF] hover:text-[#1DAEFF] hover:bg-white' : 'text-[#1DAEFF] bg-white hover:text-white hover:bg-[#1DAEFF]'} border-2 px-9 py-2 rounded-xl border-[#1DAEFF] text-[1vw] transition-all duration-300 `}>
                             LOG IN
                         </Button>
                     </Link>
