98c6cf3aeee36259831734a40ff89d6fbd7396d2
Soraya <sorayyaa1@gmail.com>
2025-11-09 20:55:19 +0330

    style(ContactUsSection): add decorative hexagon iconsto background for visual enhancement

--- src/component/Ui/Moloculs/ContactForm/ContactForm.jsx
+++ src/component/Ui/Moloculs/ContactForm/ContactForm.jsx
@@ -9,12 +9,12 @@ function ContactForm(){
             <div className="flex gap-2">
                 {
                     formInputList.map((item,index)=>(
-                        <Input key={index} placeholder={item.palaceholder} name={item.name} className="w-1/2 border-2 border-gray-200 rounded-xl py-3 text-xs px-5"/>
+                        <Input key={index} placeholder={item.palaceholder} name={item.name} className="w-1/2 border-2 border-gray-200 rounded-xl py-3 text-xs px-5 outline-none"/>
                     ))
                 }
             </div>
-            <Input placeholder="Email" name="email" className="w-full border-2 border-gray-200 rounded-xl text-xs py-3 px-5"/>
-            <textarea className="border-2 border-gray-200 rounded-xl py-2 px-5 text-xs resize-none" rows="10" cols="4" placeholder="Message ..."/>
+            <Input placeholder="Email" name="email" className="w-full border-2 border-gray-200 rounded-xl text-xs py-3 px-5 outline-none"/>
+            <textarea className="border-2 border-gray-200 rounded-xl py-2 px-5 text-xs resize-none outline-none" rows="10" cols="4" placeholder="Message ..."/>
             <div className="flex justify-end pt-2">
                 <Button type="submit" className="w-1/3 py-2 bg-[#1DAEFF] rounded-lg text-white">SEND</Button>
             </div>
--- src/component/Ui/Organims/ContactUsSection/ContactUsSection.jsx
+++ src/component/Ui/Organims/ContactUsSection/ContactUsSection.jsx
@@ -1,16 +1,47 @@
 import ContactForm from "../../Moloculs/ContactForm/ContactForm"
 import ContactInfo from "../../Moloculs/ContactUs-ContactInfo/ContactInfo"
+import Icons from "../../Atoms/Icons/Icons"
 
 function ContactUsSection(){
     return(
-        <div className="w-9/12 mx-auto grid grid-cols-3 gap-12">
-            <div className="col-span-2">
-                <ContactForm />
+        <div className="flex flex-col gap-20">
+            <div className="relative -z-10">
+                <div className="absolute -top-24">
+                    <Icons Name={'grayHexagon1-contactUs'} />
+                </div>
+                <div className="absolute -top-8 left-8">
+                    <Icons Name={'orangeHexagon2-contactUs'} />
+                </div>
+                <div className="absolute top-3 left-20">
+                    <Icons Name={'grayHexagon3-contactUs'} />
+                </div>
+                <div className="absolute top-24 left-1">
+                    <Icons Name={'grayHexagon4-contactUs'} />
+                </div>
             </div>
-            <div>
-                <ContactInfo />
+            <div className="w-9/12 mx-auto grid grid-cols-3 gap-12 pt-10">
+                <div className="col-span-2">
+                    <ContactForm />
+                </div>
+                <div>
+                    <ContactInfo />
+                </div>
+            </div>
+            <div className="relative -z-10">
+                <div>
+                    <div className="absolute -top-64 right-16">
+                        <Icons Name={'grayHexagon5-contactUs'} />
+                    </div>
+                    <div className="absolute -top-48 right-2">
+                        <Icons Name={'greenHexagon6-contactUs'} />
+                    </div>
+                    <div className="absolute -top-28 right-0">
+                        <Icons Name={'grayHexagon7-contactUs'} />
+                    </div>
+                </div>
             </div>
         </div>
+        
     )
 }
 
