085c4360e95f5185b41bbd32aa03fcc63cd919d7
ImanKhodamoradiany <Imankhodamoradiany@gmail.com>
2025-11-11 10:03:53 +0330

    Create Information In Footer

---
+++ src/component/Ui/Organims/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import FooterInfo from "../../Moloculs/FooterInfo/FooterInfo"
+
+function Footer(){
+    return(
+        <div className="w-[90%] mx-auto pt-20 pb-10">
+            <FooterInfo />
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Footer
