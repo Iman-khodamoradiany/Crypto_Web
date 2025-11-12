70027c3427c7a8ec2c5689b85dc49a3d99b2db9e
Soraya <sorayyaa1@gmail.com>
2025-11-13 01:51:47 +0330

    Refactor class and add two components for cleaner structure

---
+++ src/component/Ui/Moloculs/ChartSection/ChartSection.jsx
@@ -0,0 +1,17 @@
+import ChartComponent from "../../Moloculs/ChartComponent/ChartComponent"
+import ChartInfoBox from "../../Moloculs/ChartInfoBox/ChartInfoBox"
+
+function ChartSection(){
+    return(
+        <div className="grid grid-cols-2 gap-6 pt-4 w-11/12">
+            <div className="py-8 flex flex-col items-end ">
+                <div className="pe-6 w-10/12 ">
+                    <ChartComponent />
+                </div>
+            </div>
+            <ChartInfoBox />
+        </div>
+    )
+}
+
+export default ChartSection
--- src/component/Ui/Organims/FinancialOverviewSection/FinancialOverviewSection.jsx
+++ src/component/Ui/Organims/FinancialOverviewSection/FinancialOverviewSection.jsx
@@ -1,35 +1,15 @@
-import Icons from "../../Atoms/Icons/Icons"
-import ChartComponent from "../../Moloculs/ChartComponent/ChartComponent"
-import ChartInfoBox from "../../Moloculs/ChartInfoBox/ChartInfoBox"
 import SecurityCardInfo from "../../Moloculs/SecurityCardInfo/SecurityCardInfo"
 import HexagonsAsBgOfSecurityCardInfo from "../../Moloculs/HexagonsAsBgOfSecurityCardInfo/HexagonsAsBgOfSecurityCardInfo"
-
+import HexagonsAsBgOfChartComponent from "../../Moloculs/HexagonsAsBgOfChartComponent/HexagonsAsBgOfChartComponent"
+import ChartSection from "../../Moloculs/ChartSection/ChartSection"
 function FinancialOverViewSection(){
     return(
-        <div className="flex flex-col gap-10 dark:bg-[black]">
-           <div className="flex justify-end relative">
-               <div className="absolute -top-20 right-28 ">
-                    <Icons Name={'MediumGrayHexagon'} />
-               </div>
-               <div className="absolute -top-10 right-16">
-                   <Icons Name={'smallGreenHexagon'} />
-               </div>
-               <div className="absolute top-2 right-0">
-                    <Icons Name={'halfLargeHexagon'} />
-               </div>
+        <div className="flex flex-col gap-24 h-[1100px] dark:bg-[black]">
+           <div className="flex flex-col gap-24">
+                <HexagonsAsBgOfChartComponent />
+                <ChartSection />
            </div>
-           <div className="grid grid-cols-2 gap-6 py-4 w-11/12">
-                <div className="py-8 flex flex-col items-end ">
-                   <div className="pt-10 pe-6 w-10/12 ">
-                       <div className="flex justify-end pe-4">
-                           {/* <img src="/Tooltip.png" alt="" className="-my-5 me-4 z-10"/> */}
-                       </div>
-                       <ChartComponent />
-                   </div>
-               </div>
-               <ChartInfoBox />
-           </div>
-           <div className=" relative h-64">
+           <div className=" relative h-64 pt-[2.3vw]">
                <div className="absolute">
                    <HexagonsAsBgOfSecurityCardInfo />
                </div>
