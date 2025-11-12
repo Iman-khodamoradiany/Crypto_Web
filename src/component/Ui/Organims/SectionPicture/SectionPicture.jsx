039676a921f49b1e780d21cf029b0f55a7acdda1
ImanKhodamoradiany <Imankhodamoradiany@gmail.com>
2025-11-12 18:38:56 +0330

    Fix the ability to select other photos

--- src/component/Ui/Organims/SectionPicture/SectionPicture.jsx
+++ src/component/Ui/Organims/SectionPicture/SectionPicture.jsx
@@ -1,26 +1,20 @@
+import { useState } from "react"
 import Button from "../../Atoms/Button/Button"
 import Img from "../../Atoms/Img/Img"
+import PictureProduct from "../../Moloculs/PictursProduct/PictursProduct"
 
 function SectionPicture() {
-    const Images = [
-        { id: 1, src: '/image/Photo2.png' },
-        { id: 2, src: '/image/Photo3.png' },
-        { id: 3, src: '/image/Photo4.png' },
-        { id: 4, src: '/image/Photo5.png' },
-    ]
+
+    const [imgSelect , setImgselect] = useState('')
+
+   
     return (
-        <div className="w-full">
-            <div className="w-2/3 flex justify-centere items-start flex-col gap-2">
-                <div className="w-full">
-                    <Img className={"w-full"} src={'/image/Photo1.png'} />
+        <div className="w-full h-[650px]">
+            <div className="w-2/3 h-full flex justify-centere items-start flex-col gap-2">
+                <div className="w-full h-[75%]">
+                    <Img className={"w-full h-full"} src={imgSelect ? imgSelect : '/image/Photo1.png'} />
                 </div>
-                <div className="w-full flex justify-center items-center gap-4">
-                    {Images.map((item) => (
-                        <Button className={'w-1/4'}>
-                            <Img className={'w-full'} src={item.src} />
-                        </Button>
-                    ))}
-                </div>
+                <PictureProduct setImgselect={setImgselect} />
             </div>
         </div>
     )
