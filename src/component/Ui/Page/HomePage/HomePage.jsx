534453d8d4b022e978375174870b3dfe6daccb1e
Soraya <sorayyaa1@gmail.com>
2025-11-15 04:29:19 +0330

    feat: add NewPropertiesSection to homePage

--- src/component/Ui/Page/HomePage/HomePage.jsx
+++ src/component/Ui/Page/HomePage/HomePage.jsx
@@ -1,13 +1,14 @@
 import Header from "../../Organims/Header/Header"
 import FinancialOverViewSection from "../../Organims/FinancialOverviewSection/FinancialOverviewSection"
 import CountryShowcase from "../../Organims/CountryShowcase/CountryShowcase"
+import NewPropertiesSection from "../../Organims/NewPropertiesSection/NewPropertiesSection"
 function HomePage() {
     return (
         <div>
             <Header />
             <FinancialOverViewSection />
             <CountryShowcase />
-            <div>حجاز</div>
+            <NewPropertiesSection />
         </div>
     )
 }
